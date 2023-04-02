2eedb2492d9b08341507cc9d2a52e21b3fbeb591
Rosiealan <516308181@qq.com>
2023-04-02 19:41:17 +0800

    init

---
+++ 01_栈结构/01_实现栈结构.ts
@@ -0,0 +1,31 @@
+// class ArrayStack{
+//     private data:any[]=[]
+//     push(element:any):void{
+//         this.data.push(element)
+//     }
+//     pop():any{
+//         return this.data.pop()
+//     }
+//     peek():any{
+//         return this.data[this.data.length-1]
+//     }
+//     isEmpty():boolean{
+//         return this.data.length===0
+//     }
+//     size():number{
+//         return this.data.length
+//     }
+// }
+// const stack=new ArrayStack( )
+// stack.push('111')
+// stack.push('222')
+// stack.push(666)
+// stack.push('勇士')
+// console.log(stack);
+// stack.pop()
+// console.log(stack);
+// console.log(stack.peek())
+// console.log(stack.isEmpty());
+// console.log(stack.size());
+
+
