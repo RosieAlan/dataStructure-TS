a25c9f138cde4b4819edcc1bd0021f8341660149
“alan” <516308181@qq.com>
2023-08-20 18:29:58 +0800

    1. 栈的重构

--- 01_栈结构/01_实现栈结构.ts
+++ 01_栈结构/01_实现栈结构.ts
@@ -1,31 +1,33 @@
-// class ArrayStack{
-//     private data:any[]=[]
-//     push(element:any):void{
-//         this.data.push(element)
-//     }
-//     pop():any{
-//         return this.data.pop()
-//     }
-//     peek():any{
-//         return this.data[this.data.length-1]
-//     }
-//     isEmpty():boolean{
-//         return this.data.length===0
-//     }
-//     size():number{
-//         return this.data.length
-//     }
-// }
-// const stack=new ArrayStack( )
-// stack.push('111')
-// stack.push('222')
-// stack.push(666)
-// stack.push('勇士')
-// console.log(stack);
-// stack.pop()
-// console.log(stack);
-// console.log(stack.peek())
-// console.log(stack.isEmpty());
-// console.log(stack.size());
+class ArrayStack<T = any> {
+    private data: T[] = []
+
+    push(element: T): void {
+        this.data.push(element)
+    }
+
+    pop(): any | undefined {
+        return this.data.pop()
+    }
+
+    peek(): any {
+        return this.data[this.data.length - 1]
+    }
+
+    isEmpty(): boolean {
+        return this.data.length !== 0
+    }
+
+    size(): any {
+        return this.data.length
+    }
+}
+
+const stack1 = new ArrayStack<string>()
+
+stack1.push('aaa')
+
+const stack2 = new ArrayStack()
+stack2.push(1)
 
 
+export { }
--- 01_栈结构/02_实现栈结构(重构).ts
+++ 01_栈结构/02_实现栈结构(重构).ts
@@ -1,29 +1,27 @@
-// class ArrayStack{
-//     private data:any[]=[]
-//     push(element:any):void{
-//         this.data.push(element)
-//     }
-//     pop():any{
-//         return this.data.pop()
-//     }
-//     peek():any{
-//         return this.data[this.data.length-1]
-//     }
-//     isEmpty():boolean{
-//         return ssata.length===0
-//     }
-//     size():number{
-//         return this.data.length
-//     }
-// }
-// const stack=new ArrayStack( )
-// stack.push('111')
-// stack.push('222')
-// stack.push(666)
-// stack.push('勇士')
-// console.log(stack);
-// stack.pop()
-// console.log(stack);
-// console.log(stack.peek())
-// console.log(stack.isEmpty());
-// console.log(stack.size());
+class ArrayStack {
+    private data: any[] = []
+
+    push(element): void {
+        this.data.push(element)
+    }
+
+    pop(): any {
+        return this.data.pop()
+    }
+
+    peek(): any {
+        return this.data[this.data.length - 1]
+    }
+
+    isEmpty(): boolean {
+        return this.data.length !== 0
+    }
+
+    size(): any {
+        return this.data.length
+    }
+}
+
+const stack1 = new ArrayStack()
+
+stack1.push('aaa')
